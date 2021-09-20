70fc422dade4863770b4f362844e333a08a14af6
Yossi Ben Zaken <yossibz@gmail.com>
2021-09-20 14:07:46 +0300

    first commit

---
+++ lib/homepage.dart
@@ -0,0 +1,118 @@
+import 'package:flutter/material.dart';
+import 'package:carousel_pro/carousel_pro.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    Widget image_carousel = Container(
+      height: 200,
+      child: Carousel(
+        boxFit: BoxFit.cover,
+        images: [
+          AssetImage('images/c1.jpg'),
+          AssetImage('images/m1.jpeg'),
+          AssetImage('images/w3.jpeg'),
+          AssetImage('images/w4.jpeg'),
+          AssetImage('images/m2.jpg'),
+        ],
+        autoplay: false,
+        animationCurve: Curves.fastOutSlowIn,
+        animationDuration: Duration(milliseconds: 1000),
+      ),
+    );
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0.1,
+        backgroundColor: Colors.red,
+        title: const Text('Fashapp'),
+        actions: [
+          IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.search, color: Colors.white)),
+          IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.shopping_cart, color: Colors.white)),
+        ],
+      ),
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            UserAccountsDrawerHeader(
+              accountName: const Text('Yossi Ben Zaken'),
+              accountEmail: const Text("yossibz@gmail.com"),
+              currentAccountPicture: GestureDetector(
+                child: const CircleAvatar(
+                  backgroundColor: Colors.grey,
+                  child: Icon(
+                    Icons.person,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              decoration: const BoxDecoration(color: Colors.red),
+            ),
+            const InkWell(
+              child: ListTile(
+                title: Text('Home Page'),
+                leading: Icon(Icons.home),
+              ),
+            ),
+            const InkWell(
+              child: ListTile(
+                title: Text('My account'),
+                leading: Icon(Icons.person),
+              ),
+            ),
+            const InkWell(
+              child: ListTile(
+                title: Text('My orders'),
+                leading: Icon(Icons.shopping_basket),
+              ),
+            ),
+            const InkWell(
+              child: ListTile(
+                title: Text('Categories'),
+                leading: Icon(Icons.dashboard),
+              ),
+            ),
+            const InkWell(
+              child: ListTile(
+                title: Text('Favorites'),
+                leading: Icon(Icons.favorite),
+              ),
+            ),
+            const Divider(),
+            const InkWell(
+              child: ListTile(
+                title: Text('Settings'),
+                leading: Icon(
+                  Icons.settings,
+                  color: Colors.blue,
+                ),
+              ),
+            ),
+            const InkWell(
+              child: ListTile(
+                title: Text('About'),
+                leading: Icon(
+                  Icons.help,
+                  color: Colors.green,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+      body: ListView(
+        children: [image_carousel],
+      ),
+    );
+  }
+}
